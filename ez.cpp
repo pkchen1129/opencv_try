deee7772779452817b5e6c271bfb0761f40eced8
Po-Kang <pkang@changanus.com>
2020-06-26 12:10:13 -0400

    push

--- ez.cpp
+++ ez.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include <cv.h>
+#include <opencv/cv.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include<fstream>
+#include <vector>
 using namespace std;
 using namespace cv;
 
 int main(){
+	vector<int> params;
+	// params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	params.push_back(5);
 	cout << "try" << endl;
 	Mat im = imread("lena.jpg");
-	imshow("lena", im);
-	imwrite("hi.png", im);
+	// imshow("lena", im);
+	// imwrite("hi.png", im, params);
 	waitKey(0);
 	return 0;
 }
